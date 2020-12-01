030728788db17f332c967151ed9c78fbb752c015
ronny <ronnydemera@gmail.com>
2020-12-01 23:11:06 +0100

    Added MyToken.sol contract (yarn required)
    Created an ERC20 contract from Openzeppelin

---
+++ truffleProject/contracts/MyToken.sol
@@ -0,0 +1,15 @@
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+pragma solidity ^0.6.2;
+
+contract MyToken is ERC20, Ownable {
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) public {}
+
+    function createUser(address user) public onlyOwner {
+        require(user != address(0));
+        _mint(user, 100 * 10 ** uint(decimals()));
+    }
+}
+
