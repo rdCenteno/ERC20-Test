2ef5eb1292b29ebe1678a3a888cee388483b5664
ronny <ronnydemera@gmail.com>
2020-12-02 00:26:50 +0100

    Minor change in SC

--- truffleProject/contracts/MyToken.sol
+++ truffleProject/contracts/MyToken.sol
@@ -5,11 +5,17 @@ pragma solidity ^0.6.2;
 
 contract MyToken is ERC20, Ownable {
 
+    uint256 constant INITIAL_BALANCE = 100;
+
     constructor(string memory name, string memory symbol) ERC20(name, symbol) public {}
 
     function addBalanceToUser(address user) public onlyOwner {
         require(user != address(0));
-        _mint(user, 100 * 10 ** uint(decimals()));
+        _mint(user, INITIAL_BALANCE * 10 ** uint(decimals()));
+    }
+ 
+    function getInitialBalance() public view returns (uint256) {
+        return INITIAL_BALANCE * 10 ** uint(decimals());
     }
 }
 
