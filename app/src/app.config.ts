2964f49bba9565dd31f96c39797289202164d71c
ronny <ronnydemera@gmail.com>
2020-12-03 23:07:51 +0100

    Change copy script

--- app/src/app.config.ts
+++ app/src/app.config.ts
@@ -2,6 +2,4 @@ export class AppConfig {
     public static readonly URL_NODE = "http://localhost:8545";
     public static readonly NET_ID = 5777;
     public static readonly CONTRACT_NAME = "MyToken.json"
-
-
 }
