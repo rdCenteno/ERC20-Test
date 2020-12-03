dad923632206ff97f3fcbc9ef71f69f2381f9548
ronny <ronnydemera@gmail.com>
2020-12-03 22:11:10 +0100

    New account creation with web3 (yarn required)

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { AppConfig } from "./app.config";
+import { default as Web3 } from "web3";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const USER_NAME = "USER";
+
+class App extends React.Component<any, any> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            users: new Map<string, Account>(),
+            usersId: new Array<string>()
+        };
+        this.createUser = this.createUser.bind(this);
+    }
+
+    createUser() {
+        let usersId = this.state.usersId;
+        let usersList = this.state.users;
+        const userId = usersId.length + 1;
+        const newUser = USER_NAME + userId;
+        console.log("Creating new user", newUser);
+        const web3 = new Web3(AppConfig.URL_NODE);
+        const newAccount = web3.eth.accounts.create(web3.utils.randomHex(32));
+        console.log("The new user address is: ", newAccount.address);
+        usersList.set(newUser, newAccount);
+        usersId.push(newUser)
+        this.setState({ users: usersList, usersId: usersId });
+    }
+
+    render() {
+        const users = this.state.usersId;
+
+        const showList = users.length > 0;
+
+        return (
+            <div className="app-container">
+
+                <div className="main-container">
+
+                    <div className="button-container">
+                        <button className="btn" onClick={this.createUser}>CREATE NEW USER</button>
+                    </div>
+
+                    {showList && <div className="list-container">
+                        <h2>List of Users: </h2>
+                        <React.Fragment>
+                            <ul>
+                                {users.map((userName: string) => {
+                                    return <li key={userName}> {userName}</li>
+                                })}
+                            </ul>
+                        </React.Fragment>
+                    </div>}
+
+
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ app/src/app.config.ts
@@ -0,0 +1,4 @@
+export class AppConfig {
+    public static readonly URL_NODE = "http://localhost:8545";
+
+}
